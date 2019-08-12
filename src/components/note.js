2d3097212d0ca97a802c347dec63ae792f5e3881
DeepakRese <52799988+DeepakRese@users.noreply.github.com>
2019-08-12 14:09:19 +0530

    minor change

--- src/components/note.js
+++ src/components/note.js
@@ -16,15 +16,22 @@ const note = props => {
   if (props.notes.edit) {
     content = (
       <textarea
-        style={{ width: "100%", height: "400px", margin: "5px" }}
+        style={{ width: "100%", height: "450px", margin: "5px" }}
         onChange={e => contentChange(e.target.value)}
-      >
-        {note.content}
-      </textarea>
+        value={note.content}
+      />
     );
   }
   return (
-    <div style={{ left: "200px", position: "fixed" }}>
+    <div
+      style={{
+        left: "180px",
+        position: "fixed",
+        width: "auto",
+        right: "0px",
+        margin: "10px"
+      }}
+    >
       <Title
         notes={props.notes}
         edit={props.edit}
